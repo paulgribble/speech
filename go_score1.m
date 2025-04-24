ad025f8fee39d7a0614503602861fce65072b613
Paul Gribble <pgribble@uwo.ca>
2025-04-24 11:57:48 -0400

    fix for if max moving variance of signal is not found

--- go_score1.m
+++ go_score1.m
@@ -34,42 +34,48 @@ for i=1:n_wav
     t = 0:(1/Fs):(size(y,1)/Fs); t = t(1:end-1)';
     mv = movvar(y2, round(Fs*0.05));
     [yy,ii] = max(mv);
-    i1 = ii-round(Fs*0.500);
-    i2 = ii+round(Fs*0.500);
-    token_start(i) = i1/Fs;
-    token_end(i) = i2/Fs;
-    subplot(2,1,1)
-    tt = t(i1:i2);
-    y1 = y1(i1:i2);
-    plot(tt,y1)
-    colorbar
-    xlim([tt(1),tt(end)])
-    title(file_list(i).name, 'Interpreter', 'none')
-    subplot(2,1,2)
-    spectrogram(y(:,1), 256, 230, 256, Fs, 'yaxis')
-    colorbar('eastoutside')
-    xlim([t(i1),t(i2)])
-    hold on
-    sound(y1, Fs);
-    title('CLICK CENTRE OF SIBILANT')
-    g1 = ginput(1);
-    g1 = g1(1);
-    sib_centre(i) = g1;
-    subplot(2,1,1)
-    xline([g1, g1], 'r--', 'LineWidth', 1);
-    subplot(2,1,2)
-    xline([g1, g1], 'r--', 'LineWidth', 1);
-    [c1,skew,kurt,z,f] = ComputeCOG(y(:,1),Fs,g1);
-    c1_msg = sprintf('the spectral centroid is %.0f Hz', round(c1));
-    disp(c1_msg)
-    title(c1_msg)
-    yline([c1, c1]/1000, 'r--', 'LineWidth', 1);
-%    pause(0.5)
-    subplot(2,1,1)
-    hold off
-    subplot(2,1,2)
-    hold off
-    sib_cog(i) = c1;
+    if ii<4000
+        sprintf("skipping %s ...", fname)
+        sib_centre(i) = NaN;
+        sib_cog(i) = NaN;
+    else
+        i1 = ii-round(Fs*0.500);
+        i2 = ii+round(Fs*0.500);
+        token_start(i) = i1/Fs;
+        token_end(i) = i2/Fs;
+        subplot(2,1,1)
+        tt = t(i1:i2);
+        y1 = y1(i1:i2);
+        plot(tt,y1)
+        colorbar
+        xlim([tt(1),tt(end)])
+        title(file_list(i).name, 'Interpreter', 'none')
+        subplot(2,1,2)
+        spectrogram(y(:,1), 256, 230, 256, Fs, 'yaxis')
+        colorbar('eastoutside')
+        xlim([t(i1),t(i2)])
+        hold on
+        sound(y1, Fs);
+        title('CLICK CENTRE OF SIBILANT')
+        g1 = ginput(1);
+        g1 = g1(1);
+        sib_centre(i) = g1;
+        subplot(2,1,1)
+        xline([g1, g1], 'r--', 'LineWidth', 1);
+        subplot(2,1,2)
+        xline([g1, g1], 'r--', 'LineWidth', 1);
+        [c1,skew,kurt,z,f] = ComputeCOG(y(:,1),Fs,g1);
+        c1_msg = sprintf('the spectral centroid is %.0f Hz', round(c1));
+        disp(c1_msg)
+        title(c1_msg)
+        yline([c1, c1]/1000, 'r--', 'LineWidth', 1);
+    %    pause(0.5)
+        subplot(2,1,1)
+        hold off
+        subplot(2,1,2)
+        hold off
+        sib_cog(i) = c1;
+    end
 end
 
 filedir = filedir';
