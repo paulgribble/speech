6ad112a2c4a10d756c66573627de241d93f3031f
Paul Gribble <pgribble@uwo.ca>
2026-03-06 12:55:59 -0500

    dirname

--- go_extract_all.m
+++ go_extract_all.m
@@ -1,9 +1,10 @@
 
+dirname = '/Users/plg/Downloads/e/';
 participantList = [1,2,3,4,5,6,7,8,9,10,12,13,14];
 n = length(participantList);
 fprintf("extracting %n scored participants\n")
 for i=1:n
-    fname = sprintf("TEST1S%02d_scored.csv", participantList(i));
+    fname = sprintf("%sTEST1S%02d_scored.csv", dirname,participantList(i));
     fprintf("extracting %s ...\n", fname)
     go_extract(fname, false);
 end
--- go_plot_all.R
+++ go_plot_all.R
@@ -1,10 +1,11 @@
 
 source("go_plot_exp.R")
 
+dirname = "/Users/plg/Downloads/e/"
 participantList = c(1,2,3,4,5,6,7,8,9,10,12,13,14)
 
 for (p in participantList) {
-  fname = sprintf("TEST1S%02d_extracted.csv", p)
+  fname = sprintf("%sTEST1S%02d_extracted.csv", dirname,p)
   plot_experiment(fname)
 }
 
